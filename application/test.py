5d4c9274a5d111e870c8bee39e8a616c6005fc03
Axel Gard <axel.gard@protonmail.com>
2019-11-28 21:58:46 +0100

    started replacing old api com with lib

---
+++ application/test.py
@@ -0,0 +1,30 @@
+import alpaca_trade_api as tradeapi
+
+APCA_API_BASE_URL = "https://paper-api.alpaca.markets/"
+APCA_API_KEY_ID = ""
+APCA_API_SECRET_KEY = ""
+
+
+if __name__ == '__main__':
+    """
+    With the Alpaca API, you can check on your daily profit or loss by
+    comparing your current balance to yesterday's balance.
+    """
+
+    # First, open the API connection
+    api = tradeapi.REST(
+        APCA_API_KEY_ID,
+        APCA_API_SECRET_KEY,
+        'https://paper-api.alpaca.markets'
+    )
+
+    # Get account info
+    account = api.get_account()
+
+
+    portfolio = api.list_positions()
+    portfolio = portfolio[:1]
+    for position in portfolio:
+        #print("{} shares of {}".format(position.qty, position.unrealized_plpc))
+        #print(dir(position))
+        print(vars(position)['_raw'].keys())
--- application/traders/trader.py
+++ application/traders/trader.py
@@ -2,84 +2,103 @@ import datetime, pytz, random
 import api_controller
 import traders.stockpickr as stockpickr
 import json
+import alpaca_trade_api as tradeapi
 
 
-def nasdaq_time_old():
-    """ gets the time in us Eastern for nasdaq """
-    nyc_datetime = datetime.datetime.now(pytz.timezone('US/Eastern'))
-    fmt = '%H:%M:%S'
-    hour = nyc_datetime.strftime('%H')
-    min = nyc_datetime.strftime('%M')
-    time_set = (int(hour), int(min))
-    return(time_set)
+def authentication_header():
+    with open('key.json', 'r') as file:
+        header = json.load(file)
+    return header
+
+def api():
+    auth_header = authentication_header()
+    APCA_API_KEY_ID = str(auth_header["APCA-API-KEY-ID"])
+    APCA_API_SECRET_KEY = str(auth_header["APCA-API-SECRET-KEY"])
+    # Open the API connection
+    api = tradeapi.REST(
+        APCA_API_KEY_ID,
+        APCA_API_SECRET_KEY,
+        'https://paper-api.alpaca.markets'
+    )
+    # Get account info
+    account = api.get_account()
+    return api
 
 
 def nasdaq_open():
     """ returns if nasdaq is open """
-    link = "v2/clock"
-    response = api_controller.get_request(link)
-    dict_ = json.loads(response.text)
-    return dict_["is_open"]
+    clock = api.get_clock()
+    return clock.is_open
+
+
+def order(sym, qty, beh):
+    """ submit order and is a
+    template for order """
+    order = api().submit_order(
+        symbol=sym,
+        qty=qty,
+        side=beh,
+        type='market',
+        time_in_force='gtc')
+
+    return order
 
 
 def buy(qty, sym):
     """ buys a stock.
     takes int qty and a string sym """
-    endpoint = "v2/orders"
-    payload = stockpickr.buy_payload(qty, sym)
-    buy_response = api_controller.post_request(endpoint, payload)
-    return buy_response
+    order = order(sym, qty, 'buy')
+    return order
 
 
 def sell(qty, sym):
     """ sells a stock.
     takes int qty and a string sym"""
-    global ownd_stock
-    endpoint = "v2/positions/" + sym
-    response = api_controller.delete_request(endpoint)
-    return response
+    order = order(sym, qty, 'sell')
+    return order
 
 
 def value_of_stock(sym):
-    """ """
-    #https://data.alpaca.markets/
-    #v1/bars/day?
-    today = datetime.date.today()
-    yesterday = today - datetime.timedelta(days=1)
-    payload = {
-        "symbols": sym,
-        "limit": 1,
-        "start": yesterday.strftime("%Y-%m-%d"),
-        "end": today.strftime("%Y-%m-%d")
-    }
-    response = api_controller.get_data(payload)
-    dict_ = json.loads(response.text)
-    #{"GOOG":[{"t":1571803200,"o":1242.36,"h":1259.89,"l":1242.36,"c":1259.28,"v":814768}]}
-
-    return dict_
+    """ takes a string sym.
+    Gets and returns the stock value at close """
+    barset = api().get_barset(sym, 'day', limit=1)
+    value  = barset[sym][0].c # get stock at close
+    return value
 
 
 def get_position():
-    response = api_controller.get_request("v2/positions").text
-    lst = json.loads(response)
-    for dict_ in lst:
-        for key in dict_.keys():
+    portfolio = api().list_positions()
+    portfolio_lst = []
+    for position in portfolio:
+        raw_position = vars(position)['_raw']
+        position_dict = {}
+        for key in raw_position.keys():
             try:
-                dict_[key] = float(dict_[key])
+                position_dict[key] = float(raw_position.key)
             except ValueError:
                 continue
-    return lst
+        portfolio_lst.append(position_dict)
+    return portfolio_lst
+
+def is_tradable(sym):
+    asset = api().get_asset(sym)
+    return aapl_asset.tradable
+
+
+def nasdaq_assets():
+    active_assets = api().list_assets(status='active')
+    # Filter the assets to NASDAQ
+    return [a for a in active_assets if a.exchange == 'NASDAQ']
 
 
 def stock_position(sym):
-    url = "v1/positions/" + str(sym)
+    return api().get_position(sym)
 
-    response = api_controller.get_request(url).text
-    return json.loads(response)
 
 def ownd_stock_qty(sym):
     position = clean_position(stock_position(sym))
     return position['qty']
+
 
 def ownd_stocks():
     lst = get_position()
