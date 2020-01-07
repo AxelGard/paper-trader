e0cfe3f4f5f3bc5ad43422a7e51282ce3b261813
Axel Gard <axel.gard@protonmail.com>
2020-01-07 21:30:19 +0100

    rm config file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 from traders import randy_random, trader, cassandra_classic
-import config as conf
 import api_controller
 import json
 
 
 if __name__ == '__main__':
     print(" [*] paper-trader started ")
-    #config = conf.DevelopmentConfig()
     cassandra_classic.run_cassandra()
     #randy_random.run_randy()
